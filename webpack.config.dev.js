2e72138188220ad49260d6517ff3e8854dae3166
Carlos Abdalla <abdalla@msdev.com.br>
2017-10-25 19:20:32 -0400

    changed the webpack configuration

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,12 @@
 import webpack from 'webpack';
 import path from 'path';
 import DashboardPlugin from 'webpack-dashboard/plugin';
+import autoprefixer from 'autoprefixer';
+
+import fs from 'fs';
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 export default {
 	resolve: {
@@ -31,86 +37,90 @@ export default {
 		new DashboardPlugin()
 	],
 	module: {
+		strictExportPresence: true,
 		rules: [
 			{
-				test: /\.jsx?$/,
+				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: ['babel-loader']
 			},
 			{
-				test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-				use: ['file-loader']
-			},
-			{
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				use: [
+				oneOf: [
+					// "url" loader works like "file" loader except that it embeds assets
+					// smaller than specified limit in bytes as data URLs to avoid requests.
+					// A missing `test` is equivalent to a match.
 					{
-						loader: 'url-loader',
+						test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+						loader: require.resolve('url-loader'),
 						options: {
 							limit: 10000,
-							mimetype: 'application/font-woff'
-						}
-					}
-				]
-			},
-			{
-				test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 10000,
-							mimetype: 'application/octet-stream'
-						}
-					}
-				]
-			},
-			{
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 10000,
-							mimetype: 'image/svg+xml'
-						}
-					}
-				]
-			},
-			{
-				test: /\.(jpe?g|png|gif|ico)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]'
-						}
-					}
-				]
-			},
-			{
-				test: /(\.css|\.scss|\.sass)$/,
-				exclude: /node_modules/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true
+							name: 'static/media/[name].[hash:8].[ext]'
 						}
 					},
+					// Process JS with Babel.
 					{
-						loader: 'postcss-loader',
+						test: /\.(js|jsx)$/,
+						exclude: /node_modules/,
+						include: resolveApp('src'),
+						loader: require.resolve('babel-loader'),
 						options: {
-							plugins: () => [require('autoprefixer')],
-							sourceMap: true
+							// This is a feature of `babel-loader` for webpack (not Babel itself).
+							// It enables caching results in ./node_modules/.cache/babel-loader/
+							// directory for faster rebuilds.
+							cacheDirectory: true
 						}
 					},
+					// "postcss" loader applies autoprefixer to our CSS.
+					// "css" loader resolves paths in CSS and adds assets as dependencies.
+					// "style" loader turns CSS into JS modules that inject <style> tags.
+					// In production, we use a plugin to extract that CSS to a file, but
+					// in development "style" loader enables hot editing of CSS.
 					{
-						loader: 'sass-loader',
+						test: /\.css$/,
+						use: [
+							require.resolve('style-loader'),
+							{
+								loader: require.resolve('css-loader'),
+								options: {
+									importLoaders: 1
+								}
+							},
+							{
+								loader: require.resolve('postcss-loader'),
+								options: {
+									// Necessary for external CSS imports to work
+									// https://github.com/facebookincubator/create-react-app/issues/2677
+									ident: 'postcss',
+									plugins: () => [
+										require('postcss-flexbugs-fixes'),
+										autoprefixer({
+											browsers: [
+												'>1%',
+												'last 4 versions',
+												'Firefox ESR',
+												'not ie < 9' // React doesn't support IE8 anyway
+											],
+											flexbox: 'no-2009'
+										})
+									]
+								}
+							}
+						]
+					},
+					// "file" loader makes sure those assets get served by WebpackDevServer.
+					// When you `import` an asset, you get its (virtual) filename.
+					// In production, they would get copied to the `build` folder.
+					// This loader doesn't use a "test" so it will catch all modules
+					// that fall through the other loaders.
+					{
+						// Exclude `js` files to keep "css" loader working as it injects
+						// it's runtime that would otherwise processed through "file" loader.
+						// Also exclude `html` and `json` extensions so they get processed
+						// by webpacks internal loaders.
+						exclude: [/\.js$/, /\.html$/, /\.json$/],
+						loader: require.resolve('file-loader'),
 						options: {
-							includePaths: [path.resolve(__dirname, 'src', 'scss')],
-							sourceMap: true
+							name: 'static/media/[name].[hash:8].[ext]'
 						}
 					}
 				]
