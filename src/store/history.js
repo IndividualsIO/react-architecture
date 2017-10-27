42d6f5c524a287d5b736b379e936f7c215f4ed4b
Carlos Abdalla <abdalla@msdev.com.br>
2017-10-27 14:10:53 -0400

    tracking history

--- src/store/history.js
+++ src/store/history.js
@@ -1,7 +1,13 @@
 import createHistory from 'history/createBrowserHistory';
 
-const history = createHistory({
-	hashType: 'slash'
+const history = createHistory();
+
+/* eslint-disable no-console */
+history.listen((location, action) => {
+	console.log(
+		`The current URL is ${location.pathname}${location.search}${location.hash}`
+	);
+	console.log(`The last navigation action was ${action}`);
 });
 
 export default history;
